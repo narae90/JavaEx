eb11ac705982aaed72574f39630a21fbb44bc41a
남승균 <skyun.nam@gmail.com>
2021-07-21 09:50:42 +0900

    반복문

--- JavaEx/src/com/javaex/basics/LoopEx.java
+++ JavaEx/src/com/javaex/basics/LoopEx.java
@@ -9,8 +9,78 @@ public class LoopEx {
 //		whileGugu();
 //		doWhileEx();
 //		forGugu();
-		continueEx();
+//		continueEx();
+//		whileGuguPractice();
+//		forGuguPractice();
+//		whileStarPractice();
+//		forStarPractice();
+		breakEx();
 		System.out.println("End of Code");
+	}
+	
+	private static void breakEx() {
+		int num = 1;
+		
+		while(true) {	//	무한 루프
+			if (num % 6 == 0 &&
+				num % 14 == 0) 
+				break;	//	루프 탈출
+			num ++;
+		}
+		System.out.println(num);
+	}
+	
+	private static void forStarPractice() {
+		//	행루프
+		for (int row = 1; row <= 6; row++) {
+			for (int col = 1; col <= row; col ++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+	}
+	
+	private static void whileStarPractice() {
+		int row = 1;
+		//	행 루프
+		while(row <= 6) {
+			//	열 루프
+			int col = 1;
+			while (col <= row) {
+				System.out.print("*");
+				col++;
+			}
+			System.out.println(); //	한 행 끝난 후 개행
+			row++;
+		}
+	}
+	
+	private static void forGuguPractice() {
+		//	단루프
+		for (int dan = 2; dan <= 9; dan++) {
+			//	숫자 루프 
+			for (int num = 1; num <= 9; num++) {
+				System.out.println(dan + "*" + num + "=" + 
+									(dan * num));
+			}
+		}
+	} 
+	
+	private static void whileGuguPractice() {
+		int dan = 2;
+		//	단루프
+		while(dan <= 9) {
+			int num = 1;
+			//	숫자 루프
+			while(num <= 9) {
+				System.out.println(dan + "*" + num + "=" + 
+								(dan * num));
+				num ++;
+			}
+			
+			dan ++;
+		}
+		
 	}
 	
 	private static void continueEx() {
