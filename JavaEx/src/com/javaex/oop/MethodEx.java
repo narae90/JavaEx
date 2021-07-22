0f117d0fef3df05872b349cf18ca1086b23a3f7d
남승균 <skyun.nam@gmail.com>
2021-07-22 14:44:53 +0900

    메서드, getter/setter

--- JavaEx/src/com/javaex/oop/MethodEx.java
+++ JavaEx/src/com/javaex/oop/MethodEx.java
@@ -6,7 +6,42 @@ public class MethodEx {
 //		printMessage();
 //		printMessage("Hello, Method");
 //		printDivide(10, 0);
-		System.out.println(divide(10, 3));
+//		System.out.println(divide(10, 3));
+		System.out.println("getSum:" + getSum(new double[] {
+			1, 2, 3, 4, 5, 6, 7, 8, 9, 10	
+		}));
+		System.out.println("getSumVar:" +
+				getSumVar(1, 2, 3, 4, 5, 6, 7, 8, 9, 10));
+		printSum("고정+가변", 1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+	}
+	
+	//	고정 인수가 앞에, 가변인수 뒤에
+	private static void printSum(String message, double ...values) {
+		System.out.println(message + ":" + getSumVar(values));
+	}
+	//	매개 변수의 갯수를 모른다
+	//	방법2. 가변 인수: 변수이름 앞에 ...
+	private static double getSumVar(double ...values) {
+		//	가변인수는 배열로 변환되어 들어온다
+//		double total = 0;
+//		
+//		for (double value: values) {
+//			total += value;
+//		}
+//		
+//		return total;
+		return getSum(values);
+	}
+	//	매개 변수의 갯수를 모른다
+	//	방법1. 인수를 배열로 전달
+	private static double getSum(double[] values) {
+		double total = 0;
+		
+		for (double value: values) {
+			total += value;
+		}
+		
+		return total;
 	}
 	
 	//	입력 o, 출력 o
