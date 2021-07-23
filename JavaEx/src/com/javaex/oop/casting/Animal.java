4a37a5027616a30e78d3c487ce98930d5113fcf8
남승균 <skyun.nam@gmail.com>
2021-07-23 15:40:08 +0900

    추상 클래스와 인터페이스

---
+++ JavaEx/src/com/javaex/oop/casting/Animal.java
@@ -0,0 +1,22 @@
+package com.javaex.oop.casting;
+
+public class Animal {
+	//	필드
+	protected String name;
+	
+	//	생성자
+	public Animal(String name) {
+		super();
+		this.name = name;
+	}
+	
+	//	메서드
+	public void eat() {
+		System.out.println(name + " is eating...");
+	}
+	
+	public void walk() {
+		System.out.println(name + " is walking...");
+	}
+
+}
---
+++ JavaEx/src/com/javaex/oop/shape/v2/Drawable.java
@@ -0,0 +1,6 @@
+package com.javaex.oop.shape.v2;
+
+public interface Drawable {
+	//	내부에는 모두 추상 메서드(abstract는 붙이지 않음)
+	public void draw();
+}
