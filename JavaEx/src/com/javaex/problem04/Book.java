f055376fc2927204bd46f47041950f8db815fe7c
남승균 <skyun.nam@gmail.com>
2021-07-26 15:09:48 +0900

    Practice03-1 3, 4번 문제 Code Review

---
+++ JavaEx/src/com/javaex/problem04/Book.java
@@ -0,0 +1,42 @@
+package com.javaex.problem04;
+
+public class Book {
+    private int bookNo;
+    private String title;
+    private String author;
+    private int stateCode;
+    
+    Book(int bookNo, String title, String author) {
+    	this.bookNo = bookNo;
+    	this.title = title;
+    	this.author = author;
+    	this.stateCode = 1;	//	재고 있음
+    }
+    
+    public void rent() {
+    	if (stateCode == 1) {
+    		//	재고 있음
+    		stateCode = 0;	//	대여중
+    		System.out.printf("%s가 대여 되었습니다%n", 
+    				title);
+    	} else {
+    		//	대여중
+    		System.out.println("이미 대여중인 책입니다.");
+    	}
+    }
+    
+    public void print() {
+    	System.out.printf("%d 책 제목: %s, 작가: %s, 대여 유무: %s%n",
+    			bookNo, title, 
+    			author, stateCode == 1 ? "재고있음": "대여중");
+    }
+    
+    public int getBookNo() {
+    	return bookNo;
+    }
+    
+    
+    
+    
+    
+}
