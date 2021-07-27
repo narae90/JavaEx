66ccf5d9a92d3d48c122edc558cc6f895f4392e7
남승균 <skyun.nam@gmail.com>
2021-07-27 09:47:31 +0900

    객체 참조 복제

---
+++ JavaEx/src/com/javaex/api/objectclass/v3/Rectangle.java
@@ -0,0 +1,30 @@
+package com.javaex.api.objectclass.v3;
+
+public class Rectangle {
+	//	필드
+	private int width;
+	private int height;
+	
+	//	생성자
+	public Rectangle(int width, int height) {
+		this.width = width;
+		this.height = height;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (obj instanceof Rectangle) {
+			//	캐스팅
+			Rectangle other = (Rectangle)obj;
+			return width * height == 
+					other.width * other.height; 
+		}
+		return super.equals(obj);
+	}
+
+
+	@Override
+	public String toString() {
+		return "Rectangle [width=" + width + ", height=" + height + "]";
+	}
+}
