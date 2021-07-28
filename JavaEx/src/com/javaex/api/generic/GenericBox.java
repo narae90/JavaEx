0760d1c182e200a9acc61a114a010e3a3abb490c
남승균 <skyun.nam@gmail.com>
2021-07-28 14:45:01 +0900

    Generic, Vector

--- JavaEx/src/com/javaex/api/generic/GenericBox.java
+++ JavaEx/src/com/javaex/api/generic/GenericBox.java
@@ -2,6 +2,14 @@ package com.javaex.api.generic;
 
 //	Generic 이용
 //	T -> Type
+
+//	파라미터 갯수는 제한이 없다
+//	많이 사용되는 파라미터 관례:
+//	T: Type
+//	R: Return Type
+//	K: Key
+//	V: Value
+
 public class GenericBox<T> {
 	T content;	//	 타입 미정 상태
 
