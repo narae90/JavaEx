c3d4dc726563442f002503e6ced5f688e19161f6
남승균 <skyun.nam@gmail.com>
2021-07-29 16:15:48 +0900

    Clean Up Codes

--- JavaEx/src/com/javaex/io/bytestream/ByteArrayStreamEx.java
+++ JavaEx/src/com/javaex/io/bytestream/ByteArrayStreamEx.java
@@ -10,33 +10,33 @@ import java.util.Arrays;
 public class ByteArrayStreamEx {
 
 	public static void main(String[] args) {
-		//	입력 소스
+		// 입력 소스
 		byte[] source = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
-		//	출력 타겟
+		// 출력 타겟
 		byte[] target = null;
-		
+
 		System.out.println("입력:" + Arrays.toString(source));
 
 		InputStream bis = null;
 		OutputStream bos = null;
-		
+
 		try {
-			//	스트림 열기
+			// 스트림 열기
 			bis = new ByteArrayInputStream(source);
 			bos = new ByteArrayOutputStream();
-			
-			int data = 0;	//	입력 스트림으로부터 넘어오는 데이터 저장
-			
-			while((data = bis.read()) != -1) {
+
+			int data = 0; // 입력 스트림으로부터 넘어오는 데이터 저장
+
+			while ((data = bis.read()) != -1) {
 				System.out.println("Read Data:" + data);
-				//	출력 스트림으로 전송
-				bos.write(data); 
+				// 출력 스트림으로 전송
+				bos.write(data);
 			}
-			
-			target = ((ByteArrayOutputStream)bos).toByteArray();
+
+			target = ((ByteArrayOutputStream) bos).toByteArray();
 			System.out.println("출력:" + Arrays.toString(target));
 		} catch (IOException e) {
-			//	IO 예외 처리
+			// IO 예외 처리
 			e.printStackTrace();
 		} catch (Exception e) {
 			e.printStackTrace();
@@ -51,8 +51,3 @@ public class ByteArrayStreamEx {
 	}
 
 }
-
-
-
-
-
--- JavaEx/src/com/javaex/io/charstream/CharStreamEx.java
+++ JavaEx/src/com/javaex/io/charstream/CharStreamEx.java
@@ -8,47 +8,48 @@ import java.io.Writer;
 public class CharStreamEx {
 	static final String rootPath = System.getProperty("user.dir") + "\\files\\";
 	static String filename = rootPath + "char.txt";
-	
+
 	public static void main(String[] args) {
 //		writeTest();
 		readTest();
 	}
-	
+
 	private static void readTest() {
 		Reader freader = null;
-		
+
 		try {
 			freader = new FileReader(filename);
-			
+
 			int data = 0;
-			while((data = freader.read()) != -1) {
-				System.out.print((char)data);
+			while ((data = freader.read()) != -1) {
+				System.out.print((char) data);
 			}
-			
+
 			System.out.println();
 		} catch (Exception e) {
 			e.printStackTrace();
 		} finally {
 			try {
 				freader.close();
-			} catch (Exception e) {}
+			} catch (Exception e) {
+			}
 		}
 	}
-	
+
 	private static void writeTest() {
-		Writer fwriter = null;	//	캐릭터 기반 출력 스트림의 최고 조상
-		
+		Writer fwriter = null; // 캐릭터 기반 출력 스트림의 최고 조상
+
 		try {
-			//	출력 스트림 생성
-			fwriter = new FileWriter(filename,	//	파일명 
-									true);		//	true => 추가(append)
-			
-			//	기록
+			// 출력 스트림 생성
+			fwriter = new FileWriter(filename, // 파일명
+					true); // true => 추가(append)
+
+			// 기록
 			fwriter.write("Bit Computer\r\n");
 			fwriter.write("Java Programming\r\n");
 			fwriter.write("2021.07\r\n");
-			fwriter.flush();	//	버퍼 비우기
-			
+			fwriter.flush(); // 버퍼 비우기
+
 			System.out.println("파일을 기록했습니다.");
 		} catch (Exception e) {
 			e.printStackTrace();
@@ -56,7 +57,7 @@ public class CharStreamEx {
 			try {
 				fwriter.close();
 			} catch (Exception e) {
-				
+
 			}
 		}
 	}
