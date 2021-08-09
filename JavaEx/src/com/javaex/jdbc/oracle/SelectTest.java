7a26040e4e61f62920a9f0ef9927debac8a1ade8
남승균 <skyun.nam@gmail.com>
2021-08-09 13:52:00 +0900

    JDBC 접속 테스트, SELECT 테스트

---
+++ JavaEx/src/com/javaex/jdbc/oracle/SelectTest.java
@@ -0,0 +1,43 @@
+package com.javaex.jdbc.oracle;
+
+import java.sql.*;
+
+public class SelectTest {
+
+	public static void main(String[] args) {
+		String dburl = "jdbc:oracle:thin:@localhost:1521:xe";
+		Connection conn = null;
+		Statement stmt = null;
+		ResultSet rs = null;
+		
+		try {
+			//	1. 드라이버 로드
+			Class.forName("oracle.jdbc.driver.OracleDriver");
+			//	2. Connection
+			conn = DriverManager.getConnection(dburl, "HR", "hr");
+			//	3. 질의 객체(Statement)
+			stmt = conn.createStatement();
+			//	4. SQL 설정
+			String sql = "SELECT department_id, department_name " +
+						" FROM departments ORDER BY department_id";
+			//	5. 실행
+			rs = stmt.executeQuery(sql);
+			
+			System.out.println(rs);
+		} catch (ClassNotFoundException e) {
+			System.err.println("드라이버 로드 실패!");
+		} catch (SQLException e) {
+			System.err.println("SQL Error!");
+		} finally {
+			try {
+				rs.close();
+				stmt.close();
+				conn.close();
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+
+	}
+
+}
