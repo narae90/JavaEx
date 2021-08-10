ccb57219c4b4156ac5164ad8e5114817ee2ad2b0
김나래 <narae900@naver.com>
2021-08-10 12:56:03 +0900

    DAO

--- JavaEx/src/com/javaex/jdbc/oracle/dao/AuthorDAO.java
+++ JavaEx/src/com/javaex/jdbc/oracle/dao/AuthorDAO.java
@@ -3,11 +3,10 @@ package com.javaex.jdbc.oracle.dao;
 import java.util.List;
 
 public interface AuthorDAO {
-	public List<AuthorVO> getList(); // 전체 목록
-	public List<AuthorVO> search(String keyword); // 검색 목록
-	public AuthorVO get(Long id); // 저자 정보 확인
-	public boolean insert(AuthorVO vo); // 삽입
-	public boolean update(AuthorVO vo); // 갱신
-	public boolean selete(Long id); // 삭제
-
+	public List<AuthorVO> getList();	//	전체 목록
+	public List<AuthorVO> search(String keyword);	//	검색 목록
+	public AuthorVO get(Long id);	//	저자 정보 확인
+	public boolean insert(AuthorVO vo);	//	삽입
+	public boolean update(AuthorVO vo);	//	갱신
+	public boolean delete(Long id);	//	삭제
 }
