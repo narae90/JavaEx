a01dba88b5ff3e7122bbac6ad324c9b1a499e984
김나래 <narae900@naver.com>
2021-08-10 09:55:22 +0900

    DAO

---
+++ JavaEx/src/com/javaex/jdbc/oracle/dao/AuthorVO.java
@@ -0,0 +1,61 @@
+package com.javaex.jdbc.oracle.dao;
+
+// VO 객체
+// - 처리 로직을 갖지 않는다
+// - 순수 데이터를 가지는 객체
+// - 기본 생성자가 있어야 한다
+// - toString, equals 등은 오버라이드 하기도 한다
+public class AuthorVO {
+   // 필드
+   // VO 객체에서는 기본 타입대신 Wrapper 사용
+   private Long authorId;
+   private String authorName;
+   private String authorDesc;
+   
+   // 생성자 - 기본 생성자가 필요
+   public AuthorVO() {
+      
+   }
+   public AuthorVO(String name, String desc) {
+      authorName = name;
+      authorDesc = desc;
+      
+   }
+   public AuthorVO(Long id, String name, String desc) {
+      this(name, desc);
+      authorId = id;
+      
+   }
+   // getter & setter
+   public Long getAuthorId() {
+      return authorId;
+   }
+   public void setAuthorId(Long authorId) {
+      this.authorId = authorId;
+   }
+   public String getAuthorName() {
+      return authorName;
+   }
+   public void setAuthorName(String authorName) {
+      this.authorName = authorName;
+   }
+   public String getAuthorDesc() {
+      return authorDesc;
+   }
+   public void setAuthorDesc(String authorDesc) {
+      this.authorDesc = authorDesc;
+   }
+   // toString 
+   @Override
+   public String toString() {
+      return "AuthorVO [authorId=" + authorId + ", authorName=" + authorName + ", authorDesc=" + authorDesc + "]";
+   }
+   
+   
+   
+   
+   
+   
+   
+   
+}
