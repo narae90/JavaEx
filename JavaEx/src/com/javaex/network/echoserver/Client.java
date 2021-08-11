c807fa8016d4c63c11363f292e76beb7ac092b75
김나래 <narae900@naver.com>
2021-08-11 14:35:32 +0900

    Client: 메시지 전송, Echo Back 수신

--- JavaEx/src/com/javaex/network/echoserver/Client.java
+++ JavaEx/src/com/javaex/network/echoserver/Client.java
@@ -14,8 +14,6 @@ import java.net.InetSocketAddress;
 import java.net.Socket;
 import java.util.Scanner;
 
-
-
 public class Client {
 
 	public static void main(String[] args) {
@@ -30,47 +28,43 @@ public class Client {
 			System.out.println("[연결 요청]");
 			
 			//	접속 시도
-			InetSocketAddress server = new InetSocketAddress("172.30.1.40", 
-													10000);
+			InetSocketAddress server = new InetSocketAddress("172.30.1.40", 10000);
 			socket.connect(server);
 			System.out.println("[서버에 연결되었습니다.]");
 			
-			// 스트림 열기
+			//	스트림 열기
 			OutputStream os = socket.getOutputStream();
 			Writer osw = new OutputStreamWriter(os, "UTF-8");
 			BufferedWriter bw = new BufferedWriter(osw);
-			
 			
 			InputStream is = socket.getInputStream();
 			Reader isr = new InputStreamReader(is);
 			BufferedReader br = new BufferedReader(isr);
 			
-			// 키보드에서 메세지 입력
-			Scanner sc = new Scanner(System.in);
-			
-			// scanner에 /q 입력하면 종료
+			//	키보드에서 메시지 입력
+			Scanner scanner = new Scanner(System.in);
+			//	scanner에 /q 입력하면 종료
 			while(true) {
-				String message = sc.nextLine(); // 한 줄 입력
+				String message = scanner.nextLine(); //	한 줄 입력
 				
-				if(message.equals("/q")) {
+				if (message.equals("/q")) {
 					System.out.println("접속을 종료합니다.");
 					break;
 				}
 				
-				// 서버로 메세지 전송
+				//	서버로 메시지 전송
 				bw.write(message);
 				bw.newLine();
 				bw.flush();
-				System.out.println("서버로 전송한 메세지 :" + message);
+				System.out.println("서버로 전송한 메시지:" + message);
 				
-				String echoMsg = br.readLine(); // 한줄 읽기
-				System.out.println("서버로부터 Echo 된 메세지 : " + echoMsg);
+				String echoMsg = br.readLine();	//	한 줄 읽기
+				System.out.println("서버로부터 Echo 된 메시지:" + echoMsg);
 			}
 			
-			sc.close();
+			scanner.close();
 			br.close();
 			bw.close();
-			
 			
 			//	종료
 			System.out.println("<클라이언트 종료>");
